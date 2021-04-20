84972961c16777ec958303b430d91733ddc86172
77SWF <61841019+77SWF@users.noreply.github.com>
2021-04-20 21:39:45 +0800

    完成

--- miniprogram/pages/me/instruction/instruction.js
+++ miniprogram/pages/me/instruction/instruction.js
@@ -2,14 +2,14 @@ Page({
     data: {
         imgUrls: [
             '../../images/instruction/1.jpg',
-            '../../images/instruction/2.jpg',
-            '../../images/instruction/3.jpg',
-            '../../images/instruction/4.jpg',
-            '../../images/instruction/5.jpg',
-            '../../images/instruction/6.jpg',
-            '../../images/instruction/7.jpg',
-            '../../images/instruction/8.jpg',
-            '../../images/instruction/9.jpg'
+            '../../images/instruction/2.png',
+            '../../images/instruction/3.png',
+            '../../images/instruction/4.png',
+            '../../images/instruction/5.png',
+            '../../images/instruction/6.png',
+            '../../images/instruction/7.png',
+            '../../images/instruction/8.png',
+            '../../images/instruction/9.png'
         ]
     }
 })
